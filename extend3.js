344d486dcae666e474d4c0636018811123c6c4eb
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-04-06 17:13:42 +0200

    Create extend3.js

---
+++ extend3.js
@@ -0,0 +1,13 @@
+ let e;
+ //
+ e=Function.prototype;
+ const callable=e.bind.bind(e.call),
+ toString=callable(Object.prototype.toString);
+ //
+ function isObject(e){return toString(e)==='[object Object]'}
+ //
+ function extend(a,b,c=true){
+	const f=isObject,x=a,o=b,m=Object.keys(o);
+	if(m.length!==0){for(let p of m){const e=x[p],v=o[p];if(c && f(e) && f(v)){extend(e,v,true)}else{x[p]=v}}}
+	return x
+ }
