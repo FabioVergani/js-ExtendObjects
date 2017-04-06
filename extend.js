d396ff31399cde7b6ec372b5de1547ae180748dd
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-04-06 16:28:27 +0200

    Create extend.js

---
+++ extend.js
@@ -0,0 +1,26 @@
+function isObject(e){return toString(e)==='[object Object]'}
+//
+let e;
+//
+e=Function.prototype;
+const callable=e.bind.bind(e.call),
+toString=callable(Object.prototype.toString);
+//
+e=null;
+//
+function extend(extended,extensor,deeply=false){//extended,extensor,deeply
+ const f=isObject;
+ for(let p of Object.keys(extensor)){
+	const e=extended[p],v=extensor[p];
+	if(deeply && f(e) && f(v)){
+		extend(e,v,true)
+	}else{
+		extended[p]=v
+	}
+ };
+ return extended
+} 
+/*
+console.dir(extend({a:1},{a:2}));
+console.dir(extend({a:{num:3}},{a:2},true));
+*/
