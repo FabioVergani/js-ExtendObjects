5e29107ae0f47013b0879b7d0d6ac6a9354fc292
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-04-07 11:46:00 +0200

    Update extend3.js

--- extend3.js
+++ extend3.js
@@ -8,6 +8,6 @@
  //
  function extend(a,b,c=true){
 	const f=isObject,x=a,o=b,m=Object.keys(o);
-	if(m.length!==0){for(let p of m){const e=x[p],v=o[p];if(c && f(e) && f(v)){extend(e,v,true)}else{x[p]=v}}}
+	if(m.length!==0){for(let p of m){const e=x[p],v=o[p];if(c && f(e) && f(v)){extend(e,v,true)}else{x[p]=v}}};
 	return x
  }
